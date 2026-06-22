404594de129391a278d3b2efd73b38b36914a81a
shimodi <am.shahmoradi93@gmail.com>
2026-06-22 12:59:40 +0330

    adding concurrency

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/segmentio/kafka-go"
 )
 
 func main() {
-	x := 2
+	sleepSeconds := 5 // sleep X seconds between bursts
 
 	writer := kafka.Writer{
 		Addr:  kafka.TCP("localhost:9092"),
@@ -17,22 +18,26 @@ func main() {
 	}
 	defer writer.Close()
 
-	rate := x * 10
-	interval := time.Second / time.Duration(rate)
-
 	i := 0
 	for {
-		i++
-		msg := kafka.Message{
-			Value: []byte(fmt.Sprintf("order-%d", i)),
+		var wg sync.WaitGroup
+
+		for j := 0; j < 100; j++ {
+			wg.Add(1)
+			i++
+			go func(seq int) {
+				defer wg.Done()
+				err := writer.WriteMessages(context.Background(), kafka.Message{
+					Value: []byte(fmt.Sprintf("message %d", seq)),
+				})
+				if err != nil {
+					fmt.Println("error:", err)
+				}
+			}(i)
 		}
 
-		err := writer.WriteMessages(context.Background(), msg)
-		if err != nil {
-			fmt.Println("Error: ", err)
-		}
-		fmt.Println(msg.Value)
-
-		time.Sleep(interval)
+		wg.Wait()
+		fmt.Printf("sent 100 messages, sleeping %d seconds...\n", sleepSeconds)
+		time.Sleep(time.Duration(sleepSeconds) * time.Second)
 	}
 }
